8e4a2f9785453fd9442239e365acd20899a8274a
Joshua Salisbury <mnemolyst@gmail.com>
2016-04-18 14:49:37 -0500

    migrated to requirejs

---
+++ public/javascripts/main.js
@@ -0,0 +1,16 @@
+requirejs.config({
+    baseUrl: 'javascripts/lib',
+    paths: {
+        app: '../app',
+        jquery: 'jquery-2.2.0.min',
+        //jqueryui: 'jquery-ui-1.11.4.custom/jquery-ui.min',
+        //paper: 'paper-core',
+    },
+    shim: {
+        'Matrix2D': {
+            exports: 'Matrix2D',
+        },
+    },
+});
+
+requirejs(['order']);
